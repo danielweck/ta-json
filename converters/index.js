731d4e27d0de8dd548448d4d1d35b7e4438e11ff
danielweck <daniel.weck@gmail.com>
2017-08-03 18:28:55 +0100

    new build method

--- converters/index.js
+++ converters/index.js
@@ -3,7 +3,7 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./json"));
-__export(require("./decorators"));
-__export(require("./converters"));
+__export(require("./converter"));
+__export(require("./buffer-converter"));
+__export(require("./date-converter"));
 //# sourceMappingURL=index.js.map
